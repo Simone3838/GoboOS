147e953123e6bde842d7fada079de10517f387d6
Simone3838 <simone.paolini14@gmail.com>
2024-12-30 11:00:58 +0100

    Update boot.asm

--- src/boot.asm
+++ src/boot.asm
@@ -1,32 +1,33 @@
-; boot.asm
-[BITS 16]
-[ORG 0x7C00]
+; boot.asm - A simple bootloader
+BITS 16
+org 0x7C00
 
 start:
-    mov ax, 0x07C0
-    add ax, 288
-    mov ss, ax
-    mov sp, 4096
-
-    mov ax, 0x07C0
+    ; Set up the stack
+    cli
+    xor ax, ax
     mov ds, ax
     mov es, ax
+    mov ss, ax
+    mov sp, 0x7C00
+    sti
 
-    call load_kernel
+    ; Print a message
+    mov si, message
+print_char:
+    lodsb
+    cmp al, 0
+    je done
+    mov ah, 0x0E
+    int 0x10
+    jmp print_char
 
-load_kernel:
-    mov bx, 0x1000
-    mov dh, 0
-    mov dl, 0
-
-    mov ah, 0x02
-    mov al, 1
-    int 0x13
-    jc disk_error
-    ret
-
-disk_error:
+done:
+    ; Hang the system
     hlt
+    jmp $
 
-times 510 - ($ - $$) db 0
+message db 'Welcome to GoboOS!', 0
+
+times 510-($-$$) db 0
 dw 0xAA55
