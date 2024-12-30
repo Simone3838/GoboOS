867465f67d1b4fdfbe706b9a9b7fdacab059b74a
Simone3838 <simone.paolini14@gmail.com>
2024-12-30 11:03:45 +0100

    Update compile.sh

--- src/compile.sh
+++ src/compile.sh
@@ -1,11 +1,4 @@
-# Assemble the bootloader
 nasm -f bin boot.asm -o boot.bin
-
-# Compile the kernel
-i686-elf-gcc -ffreestanding -m32 -c kernel.cpp -o kernel.o
-
-# Link the kernel
-i686-elf-ld -o kernel.bin -T kernel.ld kernel.o --oformat binary
-
-# Create the OS image
+i386-elf-gcc -ffreestanding -c kernel.cpp -o kernel.o
+i386-elf-ld -o kernel.bin -T link.ld kernel.o
 cat boot.bin kernel.bin > os-image.bin
