35455f204e72db874e0d8c98166ac40c6854e8a5
Simone3838 <simone.paolini14@gmail.com>
2024-12-30 11:01:36 +0100

    Update kernel.cpp

--- src/kernel.cpp
+++ src/kernel.cpp
@@ -1,12 +1,9 @@
-// kernel.cpp
+// kernel.cpp - A simple kernel
 extern "C" void kernel_main() {
-    const char* message = "Hello, GoboOS!";
-    unsigned short* video_memory = (unsigned short*)0xb8000;
-    for (int i = 0; message[i] != '\0'; ++i) {
-        video_memory[i] = (video_memory[i] & 0xFF00) | message[i];
-    }
-    while (1) {
-        // Halt the CPU
-        __asm__ __volatile__("hlt");
+    const char *str = "GoboOS Kernel Initialized!";
+    char *video_memory = (char*) 0xb8000;
+    for (int i = 0; str[i] != '\0'; i++) {
+        video_memory[i * 2] = str[i];
+        video_memory[i * 2 + 1] = 0x07; // Light grey on black
     }
 }
